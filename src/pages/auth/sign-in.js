b8f3ce1dddca31fe043623b9c38c530f0e895743
Pacurar, William Michael <wpacurar@iu.edu>
2023-10-04 19:08:27 -0400

    sign-in page WIP
    updated sign-in page authentication. WORK IN PROGRESS

--- src/pages/auth/sign-in.js
+++ src/pages/auth/sign-in.js
@@ -1,37 +1,57 @@
 import { useRef, useState, useEffect } from 'react';
-import axios from '../../lib/axios';
+import { useSession, signIn } from "next-auth/react"
+import { useRouter } from "next/router";
 
-const signIn = () => {
+const SignIn = () => {
 
     const userRef = useRef();
     const errorRef = useRef();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [userRole, setUserRole] = useState('')
+
     const [errorMsg, setErrorMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+
+    const { status, data: session } = useSession();
+    const router = useRouter();
+
 
     useEffect(() => {
         userRef.current.focus();
     }, [])
 
+    // sets user role.
+    useEffect(() => {
+        if (session?.user?.role) setUserRole(session.user.role)
+    }, [session]);
+
+    //routes autnenticated user to landing page
+    useEffect(() => {
+        console.log(userRole)
+        if (status === "authenticated" && userRole === "customer")
+            router.push('../index')
+    }, [userRole])
+
+
+    // removes error message when email/password is updated
     useEffect(() => {
         setErrorMsg('');
     }, [email, password])
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post("#", JSON.stringify({ email, password }));
-            console.log(JSON.stringify(response?.data));
-            const accessToken = response?.data?.accessToken;
-            // user role (customer/host)
-            const role = response?.data?.role;
-            //send to global context, how?({ email, password, role, accessToken });
+            const request = await signIn("credentials", {
+                redirect: false,
+                email: email,
+                password: password,
+            });
+            console.log(request);
             setEmail('');
             setPassword('');
-            setSuccess(true);
         }
         // if error when login, error message will be set.
         catch (error) {
@@ -53,42 +73,31 @@ const signIn = () => {
 
     return (
         <>
-            {/* if sign-in successful, then this page is displayed and a link to landing page is given. */}
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <br />
-                    <p>
-                        <a href="../index">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    {/* error message will display here */}
-                    <p ref={errorRef} className={errorMsg ? "errormsg" : "offscreen"} aria-live="assertive">{errorMsg}</p>
+            <section>
+                {/* error message will display here */}
+                <p ref={errorRef} className={errorMsg ? "errormsg" : "offscreen"} aria-live="assertive">{errorMsg}</p>
 
-                    {/* sign-in page below */}
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        
-                        <label htmlFor="email">Email:</label>
-                        <input type="text" id="email" ref={userRef} onChange={(e) => setEmail(e.target.value)} value={email} required />
+                {/* sign-in page below */}
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit}>
 
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" ref={userRef} onChange={(e) => setEmail(e.target.value)} value={email} required />
 
-                        <button>Sign In</button>
-                    </form>
-                    <p>
-                        Don't have an account yet?<br />
-                        <span className="line">
-                            <a href="sign-up">Sign Up</a>
-                        </span>
-                    </p>
-                </section>
-            )}
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
+
+                    <button>Sign In</button>
+                </form>
+                <p>
+                    Don't have an account yet?<br />
+                    <span className="line">
+                        <a href="sign-up">Sign Up</a>
+                    </span>
+                </p>
+            </section>
         </>
     )
 }
-signIn.Layout = "auth"
-export default signIn
+SignIn.Layout = "auth"
+export default SignIn
